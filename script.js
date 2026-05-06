74274bc1613b4d4adecc725a072c78514d5a41f6
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-05-06 09:36:26 -0300

    tabela

--- script.js
+++ script.js
@@ -14,26 +14,21 @@ table.id = "tabela"
 
 let linhas = 1;
 let coluna = 1;
-
+let contadorclass = 1;
 
 for (let l = 0; l < 10; l++) {
     let row = table.insertRow(l);
     row.id = linhas;
+    row.classList.add("Rows")
     for(let c = 0; c < 18; c++) {
         let cells = row.insertCell(c);
         cells.id = coluna
+        cells.classList.add("Elementos" + contadorclass);
         mapeamentotabela.push(`${linhas}-${coluna}`);
         coluna++;
     }
     linhas++;
+    contadorclass++
     coluna = 1;
 }
-let tabela = document.querySelectorAll("#tabela tr"); 
-
-let cells = document.querySelectorAll("td")
-
-let contador = 0;
-
-cells.forEach((td, index) => {
-    
-})
+let rows = document.querySelectorAll("#tabela tr");
